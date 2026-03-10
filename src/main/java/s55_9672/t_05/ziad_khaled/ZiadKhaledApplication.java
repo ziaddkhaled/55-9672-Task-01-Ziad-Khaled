e2a92669eefc9545fd8e3a4867924185302a8331
ziaddkhaled <ziadkh2003@icloud.com>
2026-03-11 01:34:40 +0200

    Initial commit: project setup

---
+++ src/main/java/s55_9672/t_05/ziad_khaled/ZiadKhaledApplication.java
@@ -0,0 +1,13 @@
+package s55_9672.t_05.ziad_khaled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiadKhaledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiadKhaledApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s55_9672/t_05/ziad_khaled/ZiadKhaledApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_9672.t_05.ziad_khaled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiadKhaledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
